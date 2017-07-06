63c84cef1095bb079d13b8c7060ece223b938ba5
JA van Buggenum <jessievanbuggenum@gmail.com>
2017-07-06 16:59:24 +0200

    remove .R script

--- Script.R
+++
@@ -1,7 +0,0 @@
-
-mydata <- fread(file.choose(new = FALSE))  #ColoniesDraq5.csv
-
-colony_counts_perwell <- fread(file.choose(new = FALSE)) #Grid_6wells.csv
-
-image_ID_annotation <- fread(file.choose(new = FALSE)) #annotation_cellprofilerresults.csv
-
